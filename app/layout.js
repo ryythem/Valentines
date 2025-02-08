d7e5037d29d700f51ca7dfa3d73b568a23929ebb
RythemGoyal <imrythem16@gmail.com>
2025-02-09 00:45:39 +0530

    favicon working properly

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/x-icon" href="./favicon.ico" />
+        <link rel="icon" type="image/png" href="./favicon.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
